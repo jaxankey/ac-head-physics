1eb42745f1af6096ac303568a30566b5504b90eb
Jack Sankey <jack.sankey@gmail.com>
2024-01-03 12:18:59 -0500

    packaging script

---
+++ package.py
@@ -0,0 +1,30 @@
+import zipfile, os
+
+# All the files that are needed
+files = ['cockpit.lua', 'manifest.ini', 'settings.ini']
+
+# Get the version number from the manifest
+f = open('manifest.ini', 'r')
+for line in f:
+    s = line.split('=')
+    if s[0].strip() == 'VERSION': 
+        version = s[1].strip()
+        print('VERSION='+version)
+f.close()
+
+# Get the base path
+base_path = os.path.join('extension', 'lua', 'cockpit-camera', 'ac-head-physics')
+
+# Open the zip file
+zip_name = 'ac-head-physics v'+version+'.zip'
+print(zip_name)
+zip_file = zipfile.ZipFile(zip_name, 'w')
+
+# Write the files
+for file in files: 
+    print(' ', file)
+    zip_file.write(file, os.path.join(base_path, file))
+
+# Close it.
+zip_file.close()
+print('ALL DONE!')
